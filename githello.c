2ebbcd428890f9324d8d957add22cb22a2d84634
yaoyaoguo <guoyaoyao1990@qq.com>
2017-03-30 22:56:32 +0800

    test_branch2_1

--- githello.c
+++ githello.c
@@ -4,7 +4,7 @@ int main()
 	printf("hello trunk\n");
 	printf("version1\n");
 	printf("bababaaba\n");
-	printf("test_branch2\n");
+	printf("test_branch2_1\n");
 	printf("git2 developed by yaoyao\n");
 	return 0;
 }
