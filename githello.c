5a131a0003c2b89b1aa84f22877ffc43297747d6
yaoyaoguo <guoyaoyao1990@qq.com>
2017-03-30 23:01:42 +0800

    merge test_branch2_1

--- githello.c
+++ githello.c
@@ -4,10 +4,8 @@ int main()
 	printf("hello trunk\n");
 	printf("version1\n");
 	printf("bababaaba\n");
-<<<<<<< HEAD
 	printf("master\n");
-=======
->>>>>>> test_branch2
+	printf("test_branch2_1\n");
 	printf("git2 developed by yaoyao\n");
 	return 0;
 }
